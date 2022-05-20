2c081b98416e420645341d55be849a4bec103441
SophieKI024 <sophie.beranger2@gmail.com>
2022-05-20 13:58:59 +0200

    test modif return industrial_agent

--- microgrid/agents/industrial_agent.py
+++ microgrid/agents/industrial_agent.py
@@ -1,7 +1,12 @@
+
+import sys
+sys.path += ["C:\Dossiers\optim_et_energie\Git\MA4E"]
+
 import datetime
 from microgrid.environments.industrial.industrial_env import IndustrialEnv
 import numpy as np
 import pulp as pl
+
 
 class IndustrialAgent:
     def __init__(self, env: IndustrialEnv):
@@ -14,30 +19,31 @@ class IndustrialAgent:
                       previous_reward=None):
         
         #données du manager
-        consumption_prevision = state.get("consumption_prevision") #la demande de consommation
-        soc = state.get("soc") #à utiliser au 2nd run, pas aujourd'hui
-        manager_signal = state.get("manager_signal") #les prix
-        date_time = state.get("datetime") #à utiliser au 2nd run, pas aujourd'hui
+        #consumption_prevision = state.get("consumption_prevision") #la demande de consommation
+        consumption_prevision = state['consumption_prevision']
+        #soc = state.get("soc") #stock batterie
+        soc = state['soc']
+        #manager_signal = state.get("manager_signal") #les prix
+        manager_signal = state['manager_signal']
+        #date_time = state.get("datetime")
+        date_time = state['datetime']
         
         #constantes batterie
         pmax = self.env.battery.pmax #puissance maximale de la batterie
-        efficiency = self.env.battery.efficiency #rendement de la batterie, le même pour la charge et la décharge
+        efficiency = self.env.battery.efficiency #rendement batterie, mâme pour charge/decharge
         capacity = self.env.battery.capacity #charge maximale
         a_tdec = self.env.battery.soc #state[soc]
 
         #constantes temps
-
         delta_t = datetime.timedelta(minutes=30)
         H = datetime.timedelta(hours=1)
         T = self.env.nb_pdt #Nombre de périodes temporelles
         liste_temps = [t for t in range(T)]
 
         #création du problème
-
         prob = pl.LpProblem("industrial_site", pl.LpMinimize)
 
         #définition des variables
-
         a = pl.LpVariable.dicts("batterie_stock",liste_temps,0,capacity)
         l_bat_plus = pl.LpVariable.dicts("l_batterie_plus",liste_temps,0)
         l_bat_moins = pl.LpVariable.dicts("l_batterie_moins",liste_temps,0)
@@ -55,18 +61,17 @@ class IndustrialAgent:
         for t in range(T):
 
             prob += l_bat_plus[t] + l_bat_moins[t] <= pmax
-            prob += l_bat[t] == l_bat_plus[t] - l_bat_moins[t] #pas besoin de l_bat en variable pour la prochaine fois
-            prob += l_tot[t] == consumption_prevision[t] + l_bat[t] #pas besoin de l_tot en variable pour la prochaine fois
+            prob += l_bat[t] == l_bat_plus[t] - l_bat_moins[t]
+            prob += l_tot[t] == consumption_prevision[t] + l_bat[t]
             if t>0:
                 prob += a[t] == a[t-1] +(efficiency*l_bat_plus[t] - l_bat_moins[t]*1/efficiency)*delta_t/H
 
 
         prob.solve()
-
         result = self.env.action_space.sample()
         for t in range(T):
-            result[t] = l_tot[t].value()
-
+            result[t] = l_bat[t].value()
+            #print(a[t].value())
         return result
 
 
@@ -81,6 +86,7 @@ if __name__ == "__main__":
             'capacity': 100,
             'efficiency': 0.95,
             'pmax': 25,
+            #'pmin': -25,
         },
         'building': {
             'site': 1,
@@ -91,7 +97,7 @@ if __name__ == "__main__":
     cumulative_reward = 0
     now = datetime.datetime.now()
     state = env.reset(now, delta_t)
-    for i in range(N*2):
+    for i in range(2*N):
         action = agent.take_decision(state)
         state, reward, done, info = env.step(action)
         cumulative_reward += reward
@@ -99,3 +105,6 @@ if __name__ == "__main__":
             break
         print(f"action: {action}, reward: {reward}, cumulative reward: {cumulative_reward}")
         print("State: {}".format(state))
+        if reward <0:
+            break
+
